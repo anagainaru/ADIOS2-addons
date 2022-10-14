118193115cc93377d92331b5379e282014a64c4e
anagainaru <ana.gainaru@gmail.com>
2022-10-13 21:22:17 -0400

    Allow the data streaming examples to be a C and CXX project

--- DataStreaming/ADIOS_engines_py/writer.py
+++ DataStreaming/ADIOS_engines_py/writer.py
@@ -7,15 +7,15 @@ if len(sys.argv) < 3:
     exit(1)
 
 nx = int(sys.argv[1])
-shape = [nx]
-start = [0]
-count = [nx]
+shape = [nx,2]
+start = [0,0]
+count = [nx,2]
 NSteps = int(sys.argv[2])
 
 # with-as will call adios2.close on fh at the end
-with adios2.open("cfd.sst", "w", config_file="adios.xml", io_in_config_file="test") as fh:
+with adios2.open("cfd.bp", "a") as fh:
    # NSteps from application
    for i in range(0, NSteps):
-      fh.write("physical_time", np.array([100]) )
-      pressure = np.random.rand(nx)
+      fh.write("physical_time", np.array([100*i]) )
+      pressure = np.random.rand(nx,2)
       fh.write("pressure", pressure, shape, start, count, end_step=True)

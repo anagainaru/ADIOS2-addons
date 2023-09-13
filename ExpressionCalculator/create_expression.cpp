edda267123bb3a343e3b4982bb4c67aabf91c9f0
Ana Gainaru <ana.gainaru@gmail.com>
2023-09-12 23:33:32 -0400

    Change the code to hold the expression to use std::function

--- ExpressionCalculator/create_expression.cpp
+++ ExpressionCalculator/create_expression.cpp
@@ -1,34 +1,17 @@
 #include <iostream>
+#include <unordered_map>
+#include <string>
 #include <vector>
-#include <tuple>
-#include <numeric>
-#include <algorithm>
-#include <memory>
-#include <functional>
-
-template <class T>
-class Expression;
-
-template <class T>
-class Variable
-{
-    friend class IO;
-    private:
-        std::string name;
-        T value;
-    public:
-        Variable(): name("n/a"){}
-        Variable(std::string var_name, T var_value): name(var_name){ value=var_value;}
-        std::string get_name(){ return name; }
-        T get_value(){ return value; }
-};
-
+#include <algorithm> // for_each
+#include <numeric> // accumulate
+#include <functional> // function
 
 namespace detail
 {
     template <class T>
-    T AddOp (std::vector<T> values)
+    T AddOp (std::vector<T> values) 
     {
+        
         return std::accumulate(values.begin(), values.end(), 0);
     }
 
@@ -42,80 +25,34 @@ namespace detail
 }
 
 template <class T>
-class Expression
-{
-    std::vector<std::tuple<Expression<T>, Variable<T> *>> operand_list;
-    std::string name;
-    std::function<T(std::vector<T>)> op;
+class Expression{
+    std::unordered_map<std::string, std::function<T(std::vector<T>)>> FunctionList =
+    {
+            {"add", detail::AddOp<T>},
+            {"mul", detail::MulOp<T>}
+    };
 
 public:
-    Expression(){name="__NULL";}
-    Expression(std::function<T(std::vector<T>)> o): op(o){name="n/a";}
-    Expression(std::function<T(std::vector<T>)> o, std::string s):name(s), op(o){}
-
-    void addOperand(const Expression<T> &ex)
-    {
-        operand_list.push_back({ex, NULL});
-    }
-
-    void addOperand(Variable<T> *var)
-    {
-        operand_list.push_back({Expression<T>(), var});
-    }
-
-    T compute()
-    {
-        std::vector<T> values(operand_list.size());
-        for (size_t idx=0; idx < operand_list.size(); idx++)
-        {
-            if (std::get<1>(operand_list[idx]) == NULL)
-            {
-                values[idx] = std::get<0>(operand_list[idx]).compute();
-            }else
-            {
-                values[idx] = std::get<1>(operand_list[idx])->get_value();
-            }
-        }
-        return op(values);
-    }
+    std::string function;
+    Expression(std::string name): function(name){};
+    T apply_function(std::vector<T> op){ return FunctionList[function](op);}
 };
 
-class IO{
-    private:
-        std::string io_name;
-    public:
-        IO(std::string name){ io_name=name; }
-        template <class T>
-        Variable<T> DefineVariable(const std::string name, const T value)
-        {
-            Variable<T> v(name, value);
-            return v;
-        }
-};
-
-int main ()
+class StringExpression
 {
-    IO io("Test");
-    auto var1 = io.DefineVariable<float>("/coord/x", 10);
-    std::cout << "Variable name: " << var1.get_name() << ", value: " << var1.get_value() << std::endl;
+    std::vector<std::tuple<StringExpression, std::string>> function_operators;
+    std::string function_name;
+public:
+    StringExpression(): function_name("__NULL"){};
+    StringExpression(std::string name) : function_name(name){};
+};
 
-    auto var2 = io.DefineVariable<float>("/coord/y", 5);
-    std::cout << "Variable name: " << var2.get_name() << ", value: " << var2.get_value() << std::endl;
-/*
-    Expression<float> expr = io.DefineDerivedExpression<float>(name,
-        "sqrt(px^2+py^2) \n"
-        "px:/sqrt/x \n"
-        "py:/coord/y")
 
-    Expression<float> expr = io.DefineDerivedExpression<float>(name,
-        "sqrt('/coord/x'^2+'/coord/y'^2)")
- */
-    Expression<float> ex(detail::AddOp<float>, "add");
-    ex.addOperand(&var1);
-    ex.addOperand(&var2);
-    Expression<float> ex2(detail::MulOp<float>, "complex");
-    ex2.addOperand(ex);
-    ex2.addOperand(&var1);
-    std::cout << "'(var1 + var2) * var1': " << ex2.compute() << std::endl;
+
+
+int main()
+{
+    Expression<float> exp("add");
+    std::cout << "add " << exp.apply_function({1,2,3,4}) << std::endl;
     return 0;
 }

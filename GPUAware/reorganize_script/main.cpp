037cffe2b9af012babc1ce2b831a144cbcb367a3
anagainaru <ana.gainaru@gmail.com>
2024-02-21 22:54:36 -0500

    Code changes to the reorganize script

---
+++ GPUAware/reorganize_script/main.cpp
@@ -0,0 +1,44 @@
+/*
+ * Distributed under the OSI-approved Apache License, Version 2.0.  See
+ * accompanying file Copyright.txt for details.
+ *
+ * main.cpp
+ *
+ *  Created on: Mar 7, 2018
+ *      Author: Norbert Podhorszki, pnorbert@ornl.gov
+ */
+
+#include <iostream>
+#include <stdexcept>
+
+#include "utils/adios_reorganize/Reorganize.h"
+
+#if ADIOS2_USE_MPI
+#include <mpi.h>
+#endif
+
+#include <Kokkos_Core.hpp>
+
+int main(int argc, char *argv[])
+{
+#if ADIOS2_USE_MPI
+    int provided;
+    MPI_Init_thread(&argc, &argv, MPI_THREAD_MULTIPLE, &provided);
+#endif
+
+	Kokkos::initialize(argc, argv);
+    try
+    {
+        adios2::utils::Reorganize reorg(argc, argv);
+        reorg.Run();
+    }
+    catch (std::exception &e)
+    {
+        std::cout << e.what() << "\n";
+    }
+
+	Kokkos::finalize();
+#if ADIOS2_USE_MPI
+    MPI_Finalize();
+#endif
+}

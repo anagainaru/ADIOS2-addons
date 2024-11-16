0f082c5f11c4ad93dbc9484790988e91102e40f1
Ana Gainaru <ana.gainaru@gmail.com>
2024-11-15 23:25:55 -0500

    Write all variables in the original GS file in addition to the derived data

--- Campaign/source/addDerivedToGS.py
+++ Campaign/source/addDerivedToGS.py
@@ -16,16 +16,26 @@ def addDerivedToGSdata(filename):
             U = fIn.read(varUName, start=[0]*len(varU.count()), count=varU.count())
             V = fIn.read(varVName, start=[0]*len(varV.count()), count=varV.count())
 
+            otherVarNames = [i for i in fIn.available_variables() if i!=varUName and i!=varVName]
+            otherData = {}
+            for varName in otherVarNames:
+                otherData[varName] = fIn.read(varName)
+
             if needDefine:
                 outU = fOut.io.define_variable(varUName, U, U.shape, [0]*len(varU.count()), U.shape)
                 outV = fOut.io.define_variable(varVName, V, V.shape, [0]*len(varV.count()), V.shape)
                 dmo = fOut.io.define_derived_variable(
                         "derived/sumUV",
-                        "u = "+varUName+"\nv = "+varVName+"\nu+v")
+                        "u = "+varUName+"\nv = "+varVName+"\nu+v",
+                        DerivedVarType.StatsOnly)
                 needDefine = False
 
+            fOut.begin_step()
             fOut.write(outU, U)
             fOut.write(outV, V)
+            for varName in otherVarNames:
+                fOut.write(varName, otherData[varName])
+            fOut.end_step()
 
     fOut.close()
 
